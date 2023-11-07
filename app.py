e349a2bac03546594c1dcb55d5252f2325be4b33
AlexanderUrchencoRu <alexanderurchenco.ru@gmail.com>
2023-11-07 23:22:21 +0300

    remake

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#postirony
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
